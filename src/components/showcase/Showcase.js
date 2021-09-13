e163681d2ad9acc7048910f061a12b8b592123e5
kwanele <kwanelegamedze4@gmail.com>
2021-09-13 21:51:58 +0200

    made the showcase

--- src/components/showcase/Showcase.js
+++ src/components/showcase/Showcase.js
@@ -1,9 +1,25 @@
 import './showcase.css'
+import illustration from '../../images/illustration-hero.svg'
 
 function Showcase() {
     return (
-        <div>
-            <h2>showcase</h2>
+        <div className="showcase">
+            <div className="showcase__illustration">
+                <img src={illustration} alt="hero illustration" />
+            </div>
+            <div className="showcase__text">
+                <h3>A simple Bookmark Manager</h3>
+                <p>A clean and simple interface to
+                    organize your favourite websites.
+                    Open a new browser tab and see
+                    your sites load instantly.
+                    Try it for free
+                </p>
+                <div className="showcase__btns">
+                    <button>Get it on Chrome</button>
+                    <button>Get it on Firefox</button>
+                </div>
+            </div>
         </div>
     )
 }
