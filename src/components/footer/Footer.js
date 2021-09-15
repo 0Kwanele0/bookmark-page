02ec3137efbbdd8c848411cf2a4e43e715fd4939
kwanele <kwanelegamedze4@gmail.com>
2021-09-15 12:20:30 +0200

    made the footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,22 @@
 import './footer.css'
+import logoe from '../../images/logo-bookmark.png'
+
 
 function Footer() {
     return (
-        <div>
-            <h2>footer</h2>
+        <div className="footer">
+            <div className="footer__con">
+                <img className="footer__logo" src={ logoe } alt="logo" />
+                <ul className="footer__menu">
+                    <li>Features</li>
+                    <li>Pricing</li>
+                    <li>Contact</li>
+                </ul>
+                <div className="footer__icons">
+                    <img src="https://img.icons8.com/ios-glyphs/30/ffffff/facebook.png" alt="facebook"/>
+                    <img src="https://img.icons8.com/ios-glyphs/30/ffffff/twitter.png" alt="twitter"/>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/newsletter/Newsletter.js
+++ src/components/newsletter/Newsletter.js
@@ -12,7 +12,7 @@ function Newsletter() {
         <h3>35,000+ already joined</h3>
         <h2>Stay up-to-date with what we're doing</h2>
         <form onSubmit={handleSubmit(mySubmit)}>
-          <input required {...register("email")} className="news__input" type="email" />
+          <input placeholder="Enter your Email...." required {...register("email")} className="news__input" type="email" />
           
           <button className="news__btn" type="submit">Contact Us</button>
         </form>
