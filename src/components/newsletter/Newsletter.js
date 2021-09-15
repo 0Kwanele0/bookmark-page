697a736cf1e905a40183755924eb4b336f9ce56c
kwanele <kwanelegamedze4@gmail.com>
2021-09-15 11:44:17 +0200

    made the newsletter form

--- src/components/newsletter/Newsletter.js
+++ src/components/newsletter/Newsletter.js
@@ -1,10 +1,22 @@
 import './news.css'
+import { useForm } from "react-hook-form";
+
 
 function Newsletter() {
+  const { register, handleSubmit } = useForm()
+  function mySubmit(d) {
+    console.log(d)
+  }
     return (
-        <div>
-          <h2>newsletter</h2>  
-        </div>
+      <div className="news">
+        <h3>35,000+ already joined</h3>
+        <h2>Stay up-to-date with what we're doing</h2>
+        <form onSubmit={handleSubmit(mySubmit)}>
+          <input required {...register("email")} className="news__input" type="email" />
+          
+          <button className="news__btn" type="submit">Contact Us</button>
+        </form>
+      </div>
     )
 }
 
