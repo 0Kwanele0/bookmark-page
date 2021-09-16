5e69abc93d2725134a61130234438d7cce29ab63
kwanele <kwanelegamedze4@gmail.com>
2021-09-16 22:04:11 +0200

    tried to fix error message

--- src/components/newsletter/Newsletter.js
+++ src/components/newsletter/Newsletter.js
@@ -13,7 +13,6 @@ function Newsletter() {
         <h2>Stay up-to-date with what we're doing</h2>
         <form onSubmit={handleSubmit(mySubmit)}>
           <input placeholder="Enter your Email...." required {...register("email")} className="news__input" type="email" />
-          
           <button className="news__btn" type="submit">Contact Us</button>
         </form>
       </div>
